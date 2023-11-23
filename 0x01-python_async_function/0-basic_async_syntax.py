5eb4cd1ed3192f6065c923c6cdcba78484bfa75f
mcsavvy <davemcsavvii@gmail.com>
2023-11-23 10:29:13 +0000

    style: add type hint to random_delay variable

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """A simple asynchronous coroutine."""
 import asyncio
 from random import uniform
@@ -6,6 +7,6 @@ async def wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0 and
     max_delay seconds and returns it."""
-    random_delay = uniform(0, max_delay)
+    random_delay: float = uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
